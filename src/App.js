0e30d785fe57393bfac802ee0de2158bf0b54858
NoughtKin <121004366+NoughtKin@users.noreply.github.com>
2023-04-25 21:42:37 +0700

    Add the Helmet library to App.js for App to managing the content inside the <head> seciton from HTML

--- src/App.js
+++ src/App.js
@@ -1,2 +1,35 @@
 import { useState } from "react";
 import { Helmet } from "react-helmet";
+
+export default function App() {
+  return (
+    <div>
+      <Head />
+      <Body />
+    </div>
+  );
+}
+
+function Head() {
+  return (
+    <Helmet>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="stylesheet" href="style.css" />
+      <title>Cookie Jar</title>
+    </Helmet>
+  );
+}
+
+function Body() {
+  return (
+    <div className="container">
+      <h1>Cookie Jar</h1>
+      <p>
+        Write down your experiences that you've achieved or overcome as
+        "cookies".
+      </p>
+    </div>
+  );
+}
