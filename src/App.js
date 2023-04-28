783b467bfe736ef751087a06463751a08ddc60fc
NoughtKin <121004366+NoughtKin@users.noreply.github.com>
2023-04-28 09:52:54 +0700

    Added markup.json and writing functions or components

--- src/App.js
+++ src/App.js
@@ -1,36 +1,69 @@
-function FilterableCard() {
+import * as React from "react";
+// import cardExample from "./markup.json";
+
+export default function FilterableCard() {
+  const [title, setTitle] = React.useState("");
+  // const [description, setDescription] = useState("");
+  const [card, setCard] = React.useState([]);
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  // const handleDescriptionChange = (event) => {
+  //   setDescription(event.target.value);
+  // };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+
+    const nextCard = {
+      title: title,
+      description: "a",
+      date: new Date().toLocaleString(),
+    };
+
+    setCard([...card, nextCard]);
+    setTitle("");
+  };
+
   return (
     <div>
-      <FormInput />
+      <FormInput
+        onSubmitClick={() => handleClick}
+        titleChange={() => handleTitleChange}
+      />
       <SearchBar />
-      <Card />
+      {/* <CardOutput /> */}
+      <CardExample
+        title="Graduated with honors"
+        description="Graduated from college with a 3.8 GPA"
+      />
     </div>
   );
 }
 
-function FormInput() {
+function FormInput({ onSubmitClick, titleChange }) {
   return (
-    <div>
+    <div className="form-input">
       <h1>Cookie Jar</h1>
       <p>
         Write down your experiences that you've achieved or overcome as
         "cookies".
       </p>
-      <form>
-        <form>
-          <label>Title:</label>
-          <br />
-          <input />
-          <br />
-          <br />
-          <label>Description:</label>
-          <br />
-          <textarea></textarea>
-          <br />
-          <br />
-          <button>Add Cookie</button>
-        </form>
-        <hr />
+      <form onClick={onSubmitClick}>
+        <br />
+        <label>Title:</label>
+        <br />
+        <input onChange={titleChange} />
+        <br />
+        <br />
+        <label>Description:</label>
+        <br />
+        <textarea></textarea>
+        <br />
+        <br />
+        <button className="btn">Add Cookie</button>
       </form>
     </div>
   );
@@ -39,25 +72,33 @@ function FormInput() {
 function SearchBar() {
   return (
     <div>
+      <br />
       <hr />
+      <br />
       <p>Reach for them in time of need.</p>
-      <div>
+      <div className="search-bar">
         <label>Search:</label>
-        <input />
-        <button>Search</button>
+        <input className="search-input" />
+        <button className="btn">Search</button>
       </div>
     </div>
   );
 }
 
-function Card() {
+// function CardOutput() {
+//   return (
+//   );
+// }
+
+function CardExample({ title, description }) {
   return (
     <div>
-      <div className="card-output"></div>
-      <div className="card-example"></div>
+      <div className="card-title">{title}</div>
+      <div className="card-description">{description}</div>
     </div>
   );
 }
-export default function App() {
-  return <FilterableCard />;
-}
+
+// export default function App() {
+//   return <FilterableCard />;
+// }
