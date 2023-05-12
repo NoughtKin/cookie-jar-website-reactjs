cc9b5595239f23dfc1b581992f92ff4819511b84
NoughtKin <121004366+NoughtKin@users.noreply.github.com>
2023-05-12 16:59:00 +0700

    Break 'card-output' into a smaller component/file

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,5 @@
+import { CardOutput } from "./CardOutput";
+
 export function Card({ filteredCards }) {
-  return (
-    <div className="card-output">
-      {filteredCards.map((cookie, index) => (
-        <div key={index}>
-          <div className="card-title">{cookie.title}</div>
-          <div className="card-description">{cookie.description}</div>
-          <div className="card-date">{cookie.date}</div>
-        </div>
-      ))}
-    </div>
-  );
+  return <CardOutput filteredCards={filteredCards}></CardOutput>;
 }
---
+++ src/CardOutput.js
@@ -0,0 +1,13 @@
+export function CardOutput({ filteredCards }) {
+  return (
+    <div className="card-output">
+      {filteredCards.map((cookie, index) => (
+        <div key={index}>
+          <div className="card-title">{cookie.title}</div>
+          <div className="card-description">{cookie.description}</div>
+          <div className="card-date">{cookie.date}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
